9571e15b1ef504dd3a0eb117a0aa4e0079c04335
maxim koretskyi <mkoretskyi@intropro.com>
2015-08-06 13:56:01 +0300

    Started adding texts using ng-translate

--- client/js/app.js
+++ client/js/app.js
@@ -1,20 +1,78 @@
-angular.module('app', ['data', 'ngSanitize', 'angular-carousel', 'ngAnimate', 'ui.bootstrap', 'ui.router']).config(['$stateProvider', '$urlRouterProvider', '$uiViewScrollProvider', function ($stateProvider, $urlRouterProvider, $uiViewScrollProvider) {
-    "use strict";
+angular
+    .module('app', ['data', 'ngSanitize', 'angular-carousel', 'ngAnimate', 'ui.bootstrap', 'ui.router', "pascalprecht.translate"])
+    .config(['$stateProvider', '$urlRouterProvider', '$uiViewScrollProvider', '$translateProvider', "i18nLocales",
+        function ($stateProvider, $urlRouterProvider, $uiViewScrollProvider, $translateProvider, i18nLocales) {
+            "use strict";
 
-    $uiViewScrollProvider.useAnchorScroll();
+            configureRoutes();
+            configureLocalizations();
 
-    $stateProvider
-        .state('possibilities', {
-            url: '/possibilities'
-        })
-        .state('advantages', {
-            url: '/advantages'
-        })
-        .state('pricing', {
-            url: '/pricing'
-        });
-}]);
-angular.module('app').controller('app', ['$scope', '$modal', '$http', 'data', 'mobileQuery', '$anchorScroll', '$location', '$state', function ($scope, $modal, $http, data, mobileQuery, $anchorScroll, $location, $state) {
+            function configureRoutes() {
+                $uiViewScrollProvider.useAnchorScroll();
+                $stateProvider
+                    .state('features', {
+                        url: '/features'
+                    })
+                    .state('advantages', {
+                        url: '/advantages'
+                    })
+                    .state('pricing', {
+                        url: '/pricing'
+                    });
+            }
+
+            function configureLocalizations() {
+                /*jshint -W089 */
+                for (var locale in i18nLocales) {
+                    $translateProvider.translations(locale, i18nLocales[locale]);
+                }
+                $translateProvider.fallbackLanguage('ru_RU');
+
+                var languages = null;
+
+                if (navigator.languages) {
+                    languages = navigator.languages;
+                } else {
+                    var language = navigator.language || navigator.userLanguage;
+                    languages = [language];
+                }
+
+                $translateProvider.preferredLanguage(determineLanguage(languages));
+
+                function determineLanguage(languages) {
+                    // check if chinese language is present
+                    var preferredChinese = languages.some(function (language) {
+                        if (language.indexOf('zh') !== -1) {
+                            return true;
+                        } else if (language.indexOf('CN') !== -1) {
+                            return true;
+                        }
+                        return false;
+                    });
+
+                    // check if russian language is present
+                    var preferredRussian = languages.some(function (language) {
+                        if (language.indexOf('ru') !== -1) {
+                            return true;
+                        } else if (language.indexOf('RU') !== -1) {
+                            return true;
+                        }
+                        return false;
+                    });
+
+                    var preferredLangKey = 'en_US';
+                    if (preferredChinese) {
+                        preferredLangKey = 'zh_CN';
+                    } else if (preferredRussian) {
+                        preferredLangKey = 'ru_RU';
+                    }
+
+                    return preferredLangKey;
+                }
+            }
+        }]);
+
+angular.module('app').controller('app', ['$scope', '$modal', '$http', 'mobileQuery', '$anchorScroll', '$location', '$state', '$translate', '$q', function ($scope, $modal, $http, mobileQuery, $anchorScroll, $location, $state, $translate, $q) {
     "use strict";
 
     angular.extend($scope, {
@@ -97,13 +155,120 @@ angular.module('app').controller('app', ['$scope', '$modal', '$http', 'data', 'm
                     }
                 }
             });
-        }
+        },
+
+        features: [
+            {
+                "id": "reading",
+                "active": true,
+                "icon": "top__features-list__item__link_reading",
+                "subFeatures": [{id: "adaptedTexts"}, {id: "contextTranslation"}]
+            },
+            {
+                "id": "listening",
+                "active": false,
+                "icon": "top__features-list__item__link_listening",
+                "subFeatures": [{id: "podcast"}, {id: "rewind"}, {id: "transcription"}]
+            },
+            {
+                "id": "speaking",
+                "active": false,
+                "icon": "top__features-list__item__link_communication",
+                "subFeatures": [{id: "speakingPractice"}]
+            },
+            {
+                "id": "preparation",
+                "active": false,
+                "icon": "top__features-list__item__link_preparing",
+                "subFeatures": [{id: "androidApplication"}, {id: "webInterface"}]
+            },
+            {
+                "id": "efficiency",
+                "active": false,
+                "icon": "top__features-list__item__link_effective-study",
+                "subFeatures": [{id: "contentSelectionByUserLevel"}, {id: "contentSelectionByTeacher"}, {id: "lessonPlanning"}]
+            }
+        ],
+        headerMenuItems: [
+            {
+                "id": "features",
+                "state": "features"
+            },
+            {
+                "id": "advantages",
+                "state": "advantages"
+            },
+            {
+                "id": "pricing",
+                "state": "pricing"
+            }
+        ]
     });
 
-    angular.extend($scope, data, {
-        selectedTab: 0,
-        isMobile: mobileQuery.isMobile
+    $scope.$watch(function () {
+        return $translate.use();
+    }, function (newValue, prevValue) {
+        updateTranslations();
     });
+
+    function updateTranslations() {
+        var featurePromises = [];
+
+        var featuresLocalePath = "features.";
+        $scope.features.forEach(function (feature) {
+            var fd = $q.defer();
+            var titleTopResourceId = featuresLocalePath + feature.id + ".titleTop";
+            var titleBottomResourceId = featuresLocalePath + feature.id + ".titleBottom";
+
+            $translate([titleTopResourceId, titleBottomResourceId]).then(function (translation) {
+                feature.titleTop = translation[titleTopResourceId];
+                feature.titleBottom = translation[titleBottomResourceId];
+                fd.resolve();
+            });
+            featurePromises.push(fd.promise);
+
+            feature.subFeatures.forEach(function (subFeature) {
+                var sfd = $q.defer();
+                var subFeaturePath = featuresLocalePath + feature.id + ".subFeatures.";
+                var titlePath = subFeaturePath + subFeature.id + ".title";
+                var textPath = subFeaturePath + subFeature.id + ".text";
+
+                $translate([titlePath, textPath]).then(function (translation) {
+                    subFeature.title = translation[titlePath];
+                    subFeature.text = translation[textPath];
+                    sfd.resolve();
+                });
+
+                featurePromises.push(sfd);
+            });
+
+        });
+
+        var headerMenuItemsPromises = [];
+        var headerMenuItemsLocalePath = "header.menu.items.";
+        $scope.headerMenuItems.forEach(function (item) {
+            var d = $q.defer();
+            var title = headerMenuItemsLocalePath + item.id + ".title";
+
+            $translate([title]).then(function (translation) {
+                item.title = translation[title];
+                d.resolve();
+            });
+            headerMenuItemsPromises.push(d.promise);
+        });
+
+        /*
+         // not sure this is required
+         $q.all([featurePromises, headerMenuItemsPromises]).then(function () {
+         // update scope when all translations are resolved
+         });
+         */
+    }
+
+    /*angular.extend($scope, data, {
+     selectedTab: 0,
+     isMobile: mobileQuery.isMobile
+     });*/
 
     $scope.$on('$stateChangeSuccess', function (event, toState) {
         $anchorScroll.yOffset = 49;
@@ -116,13 +281,13 @@ angular.module('app').controller('app', ['$scope', '$modal', '$http', 'data', 'm
 angular.module('app').controller('getLesson', ['$scope', '$http', function ($scope, $http) {
     "use strict";
 
-    angular.extend($scope, {
-        data: {
-            name: 'domdom',
-            phone: '',
-            email: 'dom@dom'
-        }
-    });
+    /*angular.extend($scope, {
+     data: {
+     name: 'domdom',
+     phone: '',
+     email: 'dom@dom'
+     }
+     });*/
 
     angular.extend($scope, {
         onSubmit: function () {
